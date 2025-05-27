a5a26e476a2e924e498d09f88db5c35db856fca9
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-05-27 15:30:42 +0700

    fungsi asosiasi

--- Asosiasi/asosisasi.cpp
+++ Asosiasi/asosisasi.cpp
@@ -9,4 +9,13 @@ class pasien // Asosiasi
 public:
     string nama;
     vector<dokter *> daftar_dokter;
+    pasien(string pNama) : nama(pNama)
+    {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+
+    ~pasien()
+    {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
 };

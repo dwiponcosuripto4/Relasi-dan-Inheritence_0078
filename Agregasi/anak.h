091895b5e57e7f93bdb31c9b8c7398963f90f8b8
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-05-27 15:04:59 +0700

    var ibu

---
+++ Agregasi/anak.h
@@ -0,0 +1,21 @@
+#ifndef ANAK_H
+#define ANAK_H
+
+class anak
+{
+private:
+    /* data */
+public:
+    anak(/* args */);
+    ~anak();
+};
+
+anak::anak(/* args */)
+{
+}
+
+anak::~anak()
+{
+}
+
+#endif

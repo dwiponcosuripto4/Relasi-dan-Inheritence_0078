95a7749d8de03772b5ec468acffde9f760e8bc55
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-05-27 16:04:36 +0700

    asosiasi dokter

--- Asosiasi/asosisasi.cpp
+++ Asosiasi/asosisasi.cpp
@@ -58,10 +58,47 @@ void pasien::cetakDokter()
     cout << endl;
 }
 
+void dokter::tambahPasien(pasien *pPasien)
+{
+    daftar_pasien.push_back(pPasien);
+}
+
+void dokter::cetakPasien()
+{
+    cout << "Daftar pasien dari dokter \n"
+         << this->nama << "\n";
+    for (auto a : daftar_pasien)
+    {
+        cout << a->nama << "\n";
+    }
+
+    cout << endl;
+}
+
 int main()
 {
     dokter *varDokter1 = new dokter("dr.Budi");
     dokter *varDokter2 = new dokter("dr.Tono");
     pasien *varPasien1 = new pasien("Andi");
     pasien *varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+
+    varPasien1->tambahDokter(varDokter1);
+    varPasien2->tambahDokter(varDokter2);
+    varPasien1->tambahDokter(varDokter2);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
 }
